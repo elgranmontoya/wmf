bde6135ff412e2563f97cb7957f16163e761e0b2
milimetric <dandreescu@wikimedia.org>
2015-12-11 22:16:47 -0500

    Add pageview API

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='wmf',
+    packages=find_packages(),
+    version='0.1',
+    description='Tools from the Wikimedia Foundation, such as a pageview API client',
+    author='Dan Andreescu',
+    author_email='dan.andreescu@gmail.com',
+    url='https://github.com/milimetric/wmf',
+    download_url='https://github.com/milimetric/wmf/tarball/0.1',
+    keywords=['wmf', 'wikimedia', 'wikipedia', 'pageview', 'api'],
+    install_requires=[
+        'requests>=2.4',
+    ],
+    classifiers=[],
+)
